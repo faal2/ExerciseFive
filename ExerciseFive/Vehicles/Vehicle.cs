983116eb207713c41ab1299d7bc4f4a9db5d1224
faal <130977909+faal2@users.noreply.github.com>
2025-11-25 11:41:05 +0100

    Vehicle class added

---
+++ ExerciseFive/Vehicles/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseFive.Vehicles
+{
+    public abstract class Vehicle
+    {
+        public int RegisterNumber { get; }
+
+        public Color Color { get; }
+
+        public int Wheels { get; }
+        public Vehicle(int registerNumber, Color color, int wheels)
+        {
+            RegisterNumber = registerNumber;
+            Color = color;
+            Wheels = wheels;
+            
+        }
+    }
+}
