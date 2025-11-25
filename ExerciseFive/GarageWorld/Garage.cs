e7b99d0dd7a41fac394b5c3f131f1128a6008b10
faal <130977909+faal2@users.noreply.github.com>
2025-11-25 11:52:42 +0100

    added capacity and array

---
+++ ExerciseFive/GarageWorld/Garage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using ExerciseFive.Vehicles;
+
+namespace ExerciseFive.GarageWorld
+{
+    internal class Garage
+    {
+
+        public Vehicle[] Vehicles { get; }
+        public Garage(int capacity)
+        {
+            Vehicles = new Vehicle[capacity];
+
+        }
+    }
+}
