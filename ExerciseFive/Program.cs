d85158f53dcbe6c4d9dea3c7a791337eb329c11f
faal <130977909+faal2@users.noreply.github.com>
2025-11-25 11:43:38 +0100

    comment out for now

--- ExerciseFive/Program.cs
+++ ExerciseFive/Program.cs
@@ -8,10 +8,10 @@ namespace ExerciseFive
         static void Main(string[] args)
         {
             //Program starts here creates garage and ui the 2 dependencis the garage application needs.
-            var garage = new Garage();
-            var ui = new ConsoleUI();
-            var manager = new GarageManager(garage, ui);
-            manager.Run();
+            //var garage = new Garage();
+            //var ui = new ConsoleUI();
+            //var manager = new GarageManager(garage, ui);
+            //manager.Run();
             Console.WriteLine("Garage application done");
         }
     }
