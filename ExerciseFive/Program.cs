4bb3a9dc78b2996af18547e9fa963d4ff4d453ee
faal <130977909+faal2@users.noreply.github.com>
2025-11-25 10:30:27 +0100

    Add project files.

---
+++ ExerciseFive/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace ExerciseFive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program starts here creates garage and ui the 2 dependencis the garage application needs.
+            var garage = new Garage();
+            var ui = new ConsoleUI();
+            var manager = new GarageManager(garage, ui);
+            manager.Run();
+            Console.WriteLine("Garage application done");
+        }
+    }
+}
