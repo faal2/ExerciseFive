0926e544a2ed766c0d67ede439cf3be8aae0a869
faal <130977909+faal2@users.noreply.github.com>
2025-11-25 11:41:53 +0100

    Colors for vehicles  added

---
+++ ExerciseFive/Vehicles/Color.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseFive.Vehicles
+{
+    public enum Color
+    {
+        White,
+        Black,
+        Red,
+        Green,
+        Blue
+    }
+}
