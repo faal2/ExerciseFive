db0daf1bcd0eeff8f13a63a17b08614cb59e8381
faal <130977909+faal2@users.noreply.github.com>
2025-11-26 18:29:55 +0100

    .

--- ExerciseFive/Program.cs
+++ ExerciseFive/Program.cs
@@ -1,18 +1,20 @@
-﻿using System;
+﻿using ExerciseFive.GarageWorld;
+using ExerciseFive.Vehicles;
+using System;
 using System.Diagnostics.Metrics;
 
-namespace ExerciseFive
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+
             //Program starts here creates garage and ui the 2 dependencis the garage application needs.
             //var garage = new Garage();
             //var ui = new ConsoleUI();
             //var manager = new GarageManager(garage, ui);
             //manager.Run();
+
+            var garage = new Garage<Vehicle>(5);
+            var vehicle1 = new Vehicle("99", Color.Red, 1);
+
+            garage.AddVehicle(vehicle1);
+
+            garage.RemoveVehicle(vehicle1);
             Console.WriteLine("Garage application done");
-        }
-    }
-}
+
