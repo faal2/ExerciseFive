7640e591f32891829a138998f33b7963faaebe79
faal <130977909+faal2@users.noreply.github.com>
2025-11-26 18:06:04 +0100

    looks better

--- ExerciseFive/Vehicles/Bus.cs
+++ ExerciseFive/Vehicles/Bus.cs
@@ -1,20 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ExerciseFive.Vehicles
 {
-    internal class Bus : Vehicle
+    internal class Bus(string registerNumber, Color color, int seats = 25) : Vehicle(registerNumber, color, 4)
     {
 
-        private int _seats;
-
-        public Bus(string registerNumber, Color color, int seats = 25) : base(registerNumber, color, 4)
-        {
-            _seats = seats;
-        }
+        private int _seats = seats;
 
         public int NumberOfSeats ()
         {
