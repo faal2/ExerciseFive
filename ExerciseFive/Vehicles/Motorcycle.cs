0c2b96653eb0d929c762c89861156e31a11eee7e
faal <130977909+faal2@users.noreply.github.com>
2025-11-26 18:36:16 +0100

    added pillionseat into parameter

--- ExerciseFive/Vehicles/Motorcycle.cs
+++ ExerciseFive/Vehicles/Motorcycle.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace ExerciseFive.Vehicles
 {
-    internal class Motorcycle(string registerNumber, Color color) : Vehicle(registerNumber, color, 2)
+    internal class Motorcycle(string registerNumber, Color color, bool hasPillionSeat) : Vehicle(registerNumber, color, 2)
     {
-        public bool HasPillionSeat { get; set; }
+        public bool HasPillionSeat { get; set; } = hasPillionSeat;
     }
 }
