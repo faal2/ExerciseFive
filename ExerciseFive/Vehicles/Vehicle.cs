1b0b0e4bea8f13a41efeb219a153a8a098a50ddc
faal <130977909+faal2@users.noreply.github.com>
2025-11-28 08:58:10 +0100

    vehicle shouldn't validate

--- ExerciseFive/Vehicles/Vehicle.cs
+++ ExerciseFive/Vehicles/Vehicle.cs
@@ -10,40 +10,15 @@ namespace ExerciseFive.Vehicles
 {
     public abstract class Vehicle : IVehicle
     {
-        private static HashSet<string> _uniqueRegisterNumber = new();
-
-
-        private string _registerNumber;
-
 
         public Vehicle(string registerNumber, Color color, int wheels)
         {
-            RegisterNumber = registerNumber.ToLower();
+            RegisterNumber = registerNumber;
             Color = color;
             Wheels = wheels;
         }
 
-        public string RegisterNumber {
-        
-            get
-            {
-                return _registerNumber;
-            }
-            [MemberNotNull(nameof(_registerNumber))]
-            set
-            {
-                bool addRegisterNumber = _uniqueRegisterNumber.Add(value);
-                if (addRegisterNumber)
-                {
-                    _registerNumber = value;
-                }
-                else
-                {
-                    throw new ArgumentException($"This register number already exists {value}", nameof(value));
-                }
-                
-            }
-        }
+        public string RegisterNumber { get; set; }
 
         public Color Color { get; }
         public int Wheels { get; }
