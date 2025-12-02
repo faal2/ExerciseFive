0a7d3d0fde77e6ee53854c5d9073f87eaacfd87e
faal <130977909+faal2@users.noreply.github.com>
2025-12-02 08:54:44 +0100

    added is vehicle remove test

--- ExerciseFive.Tests/UnitTest1.cs
+++ ExerciseFive.Tests/UnitTest1.cs
@@ -20,5 +20,21 @@ namespace ExerciseFive.Tests
                 //Assert
                 Assert.Equal(1, garageTest.Count());
         }
+
+
+        [Fact]
+        public void Is_Vehicle_Removed()
+        {
+            //Arrange
+            var garageTest = new Garage<Vehicle>(10);
+            var hyrBilTest = new Car("001EZY", Color.Blue);
+            garageTest.AddVehicle(hyrBilTest); //because the array is private, this is the only way. Even though you are relying on another method. 
+
+            //Act
+            garageTest.RemoveVehicle(hyrBilTest);
+
+            //Assert
+            Assert.Equal(0, garageTest.Count());
+        }
     }
 }
