2f20d09a071521719caf35f31521774bf5621b32
faal <130977909+faal2@users.noreply.github.com>
2025-12-02 08:54:12 +0100

    added garage full test

---
+++ ExerciseFive.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using ExerciseFive.Vehicles;
+using ExerciseFive.GarageWorld;
+
+
+namespace ExerciseFive.Tests
+{
+    public class UnitTest1
+    {
+
+        [Fact]
+        public void Is_Garage_Full()
+        {
+                //Arrange
+                var garageTest = new Garage<Vehicle>(10);
+                var hyrBilTest = new Car("001EZY", Color.Blue);
+
+                //Act
+                garageTest.AddVehicle(hyrBilTest);
+                
+                //Assert
+                Assert.Equal(1, garageTest.Count());
+        }
+    }
+}
